80b3c2d36e9b44dac5e155eb04279ab2be2ac1df
vinnistuck <105066105+vinnistuck@users.noreply.github.com>
2024-05-09 17:51:54 +0200

    php introduction (NON SE CAPISCE NULLA)

---
+++ backend/register.php
@@ -0,0 +1,11 @@
+<?php
+
+    require_once("dbconfig.php");
+
+    if($_POST["username"] && $_POST["password"]){
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+    }
+
+
+?>
